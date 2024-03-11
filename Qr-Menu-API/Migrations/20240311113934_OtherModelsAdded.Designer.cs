a490113404472733b41f0bb195239419d88ceaa9
derslik1-11 <derslik1-11@derslik3-11-iMac.local>
2024-03-11 14:44:05 +0300

    Migrations for new models

--- Qr-Menu-API/Migrations/20240311113934_OtherModelsAdded.Designer.cs
+++ Qr-Menu-API/Migrations/20240311113934_OtherModelsAdded.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Qr_Menu_API.Data;
 
@@ -11,9 +12,10 @@ using Qr_Menu_API.Data;
 namespace Qr_Menu_API.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20240311113934_OtherModelsAdded")]
+    partial class OtherModelsAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

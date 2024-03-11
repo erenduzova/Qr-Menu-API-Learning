920f7cee6cfecbbacf4b6450b01f6d4abfbd2bbf
derslik1-11 <derslik1-11@derslik3-11-iMac.local>
2024-03-11 14:43:01 +0300

    Category, Food, RestaurantUser Models added

---
+++ Qr-Menu-API/Models/RestaurantUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Qr_Menu_API.Models
+{
+	public class RestaurantUser
+	{
+        public int RestaurantId { get; set; }
+        public string UserId { get; set; }
+
+        [ForeignKey("RestaurantId")]
+        public Restaurant? Restaurant { get; set; }
+        [ForeignKey("UserId")]
+        public ApplicationUser? ApplicationUser { get; set; }
+    }
+}
+

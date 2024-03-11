04c7cae35bf7014ac8506648936b179e7bd95349
derslik1-11 <derslik1-11@derslik3-11-iMac.local>
2024-03-11 13:40:57 +0300

    UsersController added

---
+++ Qr-Menu-API/Controllers/UsersController.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Qr_Menu_API.Data;
+using Qr_Menu_API.Models;
+
+namespace Qr_Menu_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(ApplicationContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ApplicationUser>>> GetUsers()
+        {
+          if (_context.Users == null)
+          {
+              return NotFound();
+          }
+            return await _userManager.Users.ToListAsync();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ApplicationUser>> GetApplicationUser(string id)
+        {
+            var applicationUser = await _userManager.FindByIdAsync(id);
+
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            return applicationUser;
+        }
+
+        // PUT: api/Users/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public ActionResult PutApplicationUser(string id, ApplicationUser applicationUser, string? password = null, string? currentPassword = null)
+        {
+            var existingApplicationUser =  _userManager.FindByIdAsync(id).Result;
+
+            existingApplicationUser.Email = applicationUser.Email;
+            existingApplicationUser.Name = applicationUser.Name;
+            existingApplicationUser.PhoneNumber = applicationUser.PhoneNumber;
+            existingApplicationUser.StateId = applicationUser.StateId;
+
+            _userManager.UpdateAsync(existingApplicationUser).Wait();
+
+            if (password != null && currentPassword != null)
+            {
+                IdentityResult identityResult = _userManager.ChangePasswordAsync(existingApplicationUser, currentPassword, password).Result;
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Users
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ApplicationUser>> PostApplicationUser(ApplicationUser applicationUser, string password)
+        {
+          
+            await _userManager.CreateAsync(applicationUser,password);   
+            
+            return CreatedAtAction("GetApplicationUser", new { id = applicationUser.Id }, applicationUser);
+        }
+
+        // DELETE: api/Users/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteApplicationUser(string id)
+        {
+            var applicationUser = await _userManager.FindByIdAsync(id);
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+            applicationUser.StateId = 0;
+            await _userManager.UpdateAsync(applicationUser);
+
+            return NoContent();
+        }
+
+        private bool ApplicationUserExists(string id)
+        {
+            return (_context.Users?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}

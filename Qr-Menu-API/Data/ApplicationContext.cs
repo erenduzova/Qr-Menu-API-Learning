576acfebf2c316cf94420a37b976bf24a7dfbf31
derslik1-11 <derslik1-11@derslik3-11-iMac.local>
2024-03-11 14:43:37 +0300

    Application Context edited for new models

--- Qr-Menu-API/Data/ApplicationContext.cs
+++ Qr-Menu-API/Data/ApplicationContext.cs
@@ -13,10 +13,28 @@ namespace Qr_Menu_API.Data
 
 		public DbSet<Company>? Companies { get; set; }
         public DbSet<State>? States { get; set; }
+        public DbSet<Restaurant>? Restaurants { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Food>? Foods { get; set; }
+        public DbSet<RestaurantUser>? RestaurantUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<ApplicationUser>().HasOne(u => u.State).WithMany().OnDelete(DeleteBehavior.NoAction);
+            builder.Entity<Restaurant>().HasOne(r => r.State).WithMany().OnDelete(DeleteBehavior.NoAction);
+            builder.Entity<Category>().HasOne(c => c.State).WithMany().OnDelete(DeleteBehavior.NoAction);
+            builder.Entity<Food>().HasOne(f => f.State).WithMany().OnDelete(DeleteBehavior.NoAction);
+
+
+            builder.Entity<Restaurant>().HasOne(r => r.Company).WithMany().OnDelete(DeleteBehavior.NoAction);
+            builder.Entity<Category>().HasOne(c => c.Restaurant).WithMany().OnDelete(DeleteBehavior.NoAction);
+            builder.Entity<Food>().HasOne(f => f.Category).WithMany().OnDelete(DeleteBehavior.NoAction);
+
+            builder.Entity<RestaurantUser>().HasOne(ru => ru.Restaurant).WithMany().OnDelete(DeleteBehavior.NoAction);
+            builder.Entity<RestaurantUser>().HasOne(ru => ru.ApplicationUser).WithMany().OnDelete(DeleteBehavior.NoAction);
+
+            builder.Entity<RestaurantUser>().HasKey(ru => new { ru.UserId, ru.RestaurantId });
+
             base.OnModelCreating(builder);
         }
 

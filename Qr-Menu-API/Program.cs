525c635b902e78b163cf686e13c208437faa0b57
derslik1-11 <derslik1-11@derslik3-11-iMac.local>
2024-03-13 14:58:37 +0300

    Default values added to Program.cs

--- Qr-Menu-API/Program.cs
+++ Qr-Menu-API/Program.cs
@@ -17,7 +17,7 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-        builder.Services.AddDbContext<Data.ApplicationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDatabase")));
+        builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDatabase")));
         builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
             .AddEntityFrameworkStores<ApplicationContext>().AddDefaultTokenProviders();
         builder.Services.AddAuthentication();
@@ -32,10 +32,50 @@ public class Program
             app.UseSwaggerUI();
         }
 
+        app.UseAuthentication();
         app.UseAuthorization();
-
-
+        
         app.MapControllers();
+
+        {
+            ApplicationContext? applicationContext = app.Services.CreateScope().ServiceProvider.GetService<ApplicationContext>();
+            if (applicationContext != null)
+            {
+                applicationContext.Database.Migrate();
+                if (applicationContext.States.Count() == 0)
+                {
+                    State stateDeleted = new State(0, "Deleted");
+                    applicationContext.States.Add(stateDeleted);
+                    State stateActive = new State(1, "Active");
+                    applicationContext.States.Add(stateActive);
+                    State statePassive = new State(2, "Passive");
+                    applicationContext.States.Add(statePassive);
+                }
+                applicationContext.SaveChanges();
+                RoleManager<IdentityRole>? roleManager = app.Services.CreateScope().ServiceProvider.GetService<RoleManager<IdentityRole>>();
+                if (roleManager != null)
+                {
+                    if (roleManager.Roles.Count() == 0)
+                    {
+                        IdentityRole administratorRole = new IdentityRole("Administrator");
+                        roleManager.CreateAsync(administratorRole).Wait();
+                        IdentityRole companyAdministratorRole = new IdentityRole("CompanyAdministrator");
+                        roleManager.CreateAsync(companyAdministratorRole).Wait();
+                    }
+                }
+                UserManager<ApplicationUser>? userManager = app.Services.CreateScope().ServiceProvider.GetService<UserManager<ApplicationUser>>();
+                if (userManager != null)
+                {
+                    if (roleManager.Roles.Count() == 0)
+                    {
+                        ApplicationUser applicationUser = new ApplicationUser();
+                        applicationUser.UserName = "Administrator";
+                        userManager.CreateAsync(applicationUser, "Admin123!").Wait();
+                        userManager.AddToRoleAsync(applicationUser, "Administrator").Wait();
+                    }
+                }
+            }
+        }
 
         app.Run();
     }

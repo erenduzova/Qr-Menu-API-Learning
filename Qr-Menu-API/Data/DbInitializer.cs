9e52b33f79e1880008b99c0040f76ee3bca299b0
derslik1-11 <derslik1-11@derslik3-11-iMac.local>
2024-03-14 14:08:17 +0300

    Default starting data moved to seperate file named DbIninalizer.cs

---
+++ Qr-Menu-API/Data/DbInitializer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Qr_Menu_API.Models;
+
+namespace Qr_Menu_API.Data
+{
+	public class DbInitializer
+	{
+		private readonly ApplicationContext _applicationContext;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public DbInitializer(ApplicationContext applicationContext, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+		{
+			_applicationContext = applicationContext;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+
+		public void Initialize()
+        {
+            if (_applicationContext != null)
+            {
+                _applicationContext.Database.Migrate();
+                if (!_applicationContext.States.Any())
+                {
+                    State stateDeleted = new State(0, "Deleted");
+                    _applicationContext.States.Add(stateDeleted);
+                    State stateActive = new State(1, "Active");
+                    _applicationContext.States.Add(stateActive);
+                    State statePassive = new State(2, "Passive");
+                    _applicationContext.States.Add(statePassive);
+                }
+                if (!_applicationContext.Companies.Any())
+                {
+                    Company company = new Company();
+                    company.AddressDetails = "adres";
+                    company.EMail = "abc@def.com";
+                    company.Name = "Company";
+                    company.Phone = "1112223344";
+                    company.PostalCode = "12345";
+                    company.RegisterDate = DateTime.Today;
+                    company.StateId = 1;
+                    company.TaxNumber = "11111111111";
+                    _applicationContext.Companies.Add(company);
+                    _applicationContext.SaveChanges();
+                    if (_roleManager != null)
+                    {
+                        if (!_roleManager.Roles.Any())
+                        {
+                            IdentityRole administratorRole = new IdentityRole("Administrator");
+                            _roleManager.CreateAsync(administratorRole).Wait();
+                            IdentityRole companyAdministratorRole = new IdentityRole("CompanyAdministrator");
+                            _roleManager.CreateAsync(companyAdministratorRole).Wait();
+                        }
+                    }
+                    if (_userManager != null)
+                    {
+                        if (!_userManager.Users.Any())
+                        {
+                            if (company != null)
+                            {
+                                ApplicationUser applicationUser = new ApplicationUser();
+                                applicationUser.UserName = "Administrator";
+                                applicationUser.CompanyId = company.Id;
+                                applicationUser.Name = "Administrator";
+                                applicationUser.Email = "abc@def.com";
+                                applicationUser.PhoneNumber = "1112223344";
+                                applicationUser.RegisterDate = DateTime.Today;
+                                applicationUser.StateId = 1;
+
+                                _userManager.CreateAsync(applicationUser, "Admin123!").Wait();
+                                _userManager.AddToRoleAsync(applicationUser, "Administrator").Wait();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
+
--- Qr-Menu-API/Program.cs
+++ Qr-Menu-API/Program.cs
@@ -13,6 +13,7 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
+
         // Add services to the container.
 
         builder.Services.AddControllers();
@@ -39,68 +40,11 @@ public class Program
         
         app.MapControllers();
 
-        {
-            ApplicationContext? applicationContext = app.Services.CreateScope().ServiceProvider.GetService<ApplicationContext>();
-            if (applicationContext != null)
-            {
-                applicationContext.Database.Migrate();
-                if (!applicationContext.States.Any())
-                {
-                    State stateDeleted = new State(0, "Deleted");
-                    applicationContext.States.Add(stateDeleted);
-                    State stateActive = new State(1, "Active");
-                    applicationContext.States.Add(stateActive);
-                    State statePassive = new State(2, "Passive");
-                    applicationContext.States.Add(statePassive);
-                }
-                if (!applicationContext.Companies.Any())
-                {
-                    Company company = new Company();
-                    company.AddressDetails = "adres";
-                    company.EMail = "abc@def.com";
-                    company.Name = "Company";
-                    company.Phone = "1112223344";
-                    company.PostalCode = "12345";
-                    company.RegisterDate = DateTime.Today;
-                    company.StateId = 1;
-                    company.TaxNumber = "11111111111";
-                    applicationContext.Companies.Add(company);
-                    applicationContext.SaveChanges();
-                    RoleManager<IdentityRole>? roleManager = app.Services.CreateScope().ServiceProvider.GetService<RoleManager<IdentityRole>>();
-                    if (roleManager != null)
-                    {
-                        if (!roleManager.Roles.Any())
-                        {
-                            IdentityRole administratorRole = new IdentityRole("Administrator");
-                            roleManager.CreateAsync(administratorRole).Wait();
-                            IdentityRole companyAdministratorRole = new IdentityRole("CompanyAdministrator");
-                            roleManager.CreateAsync(companyAdministratorRole).Wait();
-                        }
-                    }
-                    UserManager<ApplicationUser>? userManager = app.Services.CreateScope().ServiceProvider.GetService<UserManager<ApplicationUser>>();
-                    if (userManager != null)
-                    {
-                        if (!userManager.Users.Any())
-                        {
-                            if (company != null)
-                            {
-                                ApplicationUser applicationUser = new ApplicationUser();
-                                applicationUser.UserName = "Administrator";
-                                applicationUser.CompanyId = company.Id;
-                                applicationUser.Name = "Administrator";
-                                applicationUser.Email = "abc@def.com";
-                                applicationUser.PhoneNumber = "1112223344";
-                                applicationUser.RegisterDate = DateTime.Today;
-                                applicationUser.StateId = 1;
-
-                                userManager.CreateAsync(applicationUser, "Admin123!").Wait();
-                                userManager.AddToRoleAsync(applicationUser, "Administrator").Wait();
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        ApplicationContext? applicationContext = app.Services.CreateScope().ServiceProvider.GetService<ApplicationContext>();
+        UserManager<ApplicationUser>? userManager = app.Services.CreateScope().ServiceProvider.GetService<UserManager<ApplicationUser>>();
+        RoleManager<IdentityRole>? roleManager = app.Services.CreateScope().ServiceProvider.GetService<RoleManager<IdentityRole>>();
+        DbInitializer dbInitializer = new DbInitializer(applicationContext, userManager, roleManager);
+        dbInitializer.Initialize();
 
         app.Run();
     }
